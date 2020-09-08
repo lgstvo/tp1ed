8c10f5671c0531470d95570344878a52cf4d809e
Luís Gustavo <32440675+lgstvo@users.noreply.github.com>
2020-09-08 17:37:42 -0300

    Add files via upload

---
+++ luis_silva/src/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <iostream>
+
+int main() {
+  int num_frotas;
+  scanf("%d",&num_frotas);
+  int i;
+  for(i=0; i < num_frotas; i++){
+    int id_nave;
+    scanf("%d",&id_nave);
+    //desenvolver o código para inserção correta das naves que são lidas
+  }
+  int operacao;
+  while(scanf("%d", &operacao) != EOF) {
+    //desenvolver o código relacionado as operações a serem realizadas
+  }
+  return 0;
+}
